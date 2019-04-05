26b543cbb7b389bbdc69bc69d934815ae4f6ea37
serromanoff <serromanoff2015@yandex.ru>
2019-04-06 02:52:23 +0400

    test from serro

---
+++ src/main/java/ru/liveproduction/victoria/api/Game.java
@@ -0,0 +1,11 @@
+package ru.liveproduction.victoria.api;
+
+public class Game {
+    List <user> users;
+    int index_play_user;
+    int max_user;
+    Pack pack_q;
+    int time_read;
+    int time_write;
+
+}
---
+++ src/main/java/ru/liveproduction/victoria/api/Pack.java
@@ -0,0 +1,7 @@
+package ru.liveproduction.victoria.api;
+
+public class Pack {
+    int id_pack;
+    Map <Categor, Map<Int, List<quest>>>;
+    Pack_complex <int comp>;
+}
