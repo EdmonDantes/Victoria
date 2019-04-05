06c42cafda1a15ad5d81ac3489191162c55f9516
Loginov Ilya <dantes2104@gmail.com>
2019-04-05 22:35:28 +0400

    First commit

---
+++ src/main/java/ru/liveproduction/victoria/api/User.java
@@ -0,0 +1,21 @@
+/*
+Copyright © 2019 Ilya Loginov. All rights reserved. 
+Please email dantes2104@gmail.com if you would like permission to do something with the contents of this repository
+
+Created dantes on 05.04.19 22:33
+*/
+
+package ru.liveproduction.victoria.api;
+
+public class User {
+
+    public int getId(){}
+
+    public String getName(){}
+
+    public String getMacAddress(){}
+
+    public boolean isPackAdded(int packId) {}
+
+    public long getRegistrationTime(){}
+}
